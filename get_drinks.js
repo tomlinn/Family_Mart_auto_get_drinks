02460be9fdc891cb126519de64727ba581e9ec01
TomLin <33810960+tomlinn@users.noreply.github.com>
2020-06-17 17:19:59 +0800

    Create get_drinks.js

---
+++ get_drinks.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         全家飲料抽獎
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  just a little tools
+// @author       Tom Lin
+// @match        https://game.family.com.tw/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    getRotaryData();
+})();
